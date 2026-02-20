fbc3fd2d6e220a72737558174a0657189c15862f
Kelli Kirk <kelli.kirk@voco.ee>
2026-02-20 11:36:52 +0200

    Create connection to database

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const dotenv = require ('dotenv')
 const express = require ("express")
 const app = express()
+dotenv.config()
 
 // parse requests of content type - application/json
 app.use(express.json())
 
 // parse requests of content type - application/x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}))
+
+// connect to database
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+} )
+
+sequelize.authenticate()
+  .then(() => console.log('Andmebaas ühendatud!'))
+  .catch(err => console.error('Viga:', err));
 
 // simple route
 app.get("/", (req, res) => {
@@ -16,3 +29,4 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
 } )
+
