f810c0935ce10ff061f58208f5f373954eef871e
Kelli Kirk <kelli.kirk@voco.ee>
2026-02-26 11:02:20 +0200

    Created new article

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const authorRouter = require('./routes/author');
 app.use('/', articleRouter)
 app.use('/article', articleRouter)
 app.use('/author', authorRouter);
+app.use('admin/article,', articleRouter)
 
 // listen requests
 app.listen(3000, () => {
